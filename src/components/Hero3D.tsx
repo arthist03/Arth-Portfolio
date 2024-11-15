061c0a3512152c941b41c6fcd4bc4a879855dfdb
Arth Kadiya <arthpk09@gmail.com>
2024-11-15 16:35:56 +0530

    first commit

--- src/components/Hero3D.tsx
+++ src/components/Hero3D.tsx
@@ -1,7 +1,6 @@
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import DeskScene from './DeskScene';
 import LoadingSpinner from './LoadingSpinner';
 
 const Hero3D = () => {
