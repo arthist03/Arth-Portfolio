9f82d1048452e53099a1f6efbcdfe5a2137ccac9
Arth Kadiya <arthpk09@gmail.com>
2024-11-16 15:13:28 +0530

    alsomost Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,9 +29,8 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            {/* Corrected Image Path */}
             <img
-              src="/AK.jpg"
+              src="/Icon/AK.jpg"
               alt="Portfolio"
               className="w-10 h-10 rounded-full"
             />
